2cabb23d8910d44d1d31fc822d70bb0dcb25cb9c
pedrotres <52545592+pedrotres@users.noreply.github.com>
2021-02-01 23:53:58 -0300

    Initial commit

---
+++ test/app.test.js
@@ -0,0 +1,12 @@
+const supertest = require("supertest");
+
+const request = require('supertest');
+
+const app = require('../src/app');
+
+test('deve responder na raiz', () => {
+    return request(app).get('/')
+        .then((res) => {
+            expect(res.status).toBe(200);
+        });
+});
---
+++ test/jest.test.js
@@ -0,0 +1,6 @@
+const { TestScheduler } = require("jest");
+
+test('devo conhecer as primeiras assertivas do jest ', ()=> {
+    let number = null;
+    expect(number).toBeNull();
+});
