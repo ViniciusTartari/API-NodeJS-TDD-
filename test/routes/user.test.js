55098a09f21d42cd61b9709d474f2e63bc1ee8d1
pedrotres <52545592+pedrotres@users.noreply.github.com>
2021-02-03 23:54:28 -0300

    Refatoração
    refatorado para utilizar o postgres e implementado log das rotas.

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,19 +1,19 @@
 const request = require('supertest');
 
-const app = require('../src/app');
+const app = require('../../src/app');
 
 test('Deve listar todos os usuarios', () => {
     return request(app).get('/users')
         .then((res) => {
             expect(res.status).toBe(200);
-            expect(res.body).toHaveLength(1);
-            expect(res.body[0]).toHaveProperty('name', 'John Doe');
+            expect(res.body.length).toBeGreaterThan(0);
         });        
 });
 
-test('Deve inserir um novo usuario', () => {
+test.only('Deve inserir um novo usuario', () => {
+    const mail = `${Date.now()}@gmail.com`;
     return request(app).post('/users')
-        .send({name : 'Walter White', mail: 'walter@gmail.com'})
+        .send({name : 'Walter White', mail, passwd: '123456'})
         .then((res) => {
             expect(res.status).toBe(201);
             expect(res.body.name).toBe('Walter White');
